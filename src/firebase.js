d551384ce6806200ab31b6a6e7e01187c7c6feff
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-12-18 19:55:18 -0800

    The start of intigrating firebase into portfolio

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// import { initializeApp } from 'firebase/app'
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-// import { getStorage } from 'firebase/storage'
-// import { getFirestore } from 'firebase/firestore/lite'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
 
-// const firebaseConfig = {
-//   apiKey: '',
-//   authDomain: '',
-//   projectId: '',
-//   storageBucket: '',
-//   messagingSenderId: '',
-//   appId: '',
-// }
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// const app = initializeApp(firebaseConfig)
-// export const auth = getAuth()
-// const provider = new GoogleAuthProvider()
-// export const db = getFirestore(app)
-// export const storage = getStorage(app)
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBo80O_9JicIhcY6OF02W84Dsr7y5a__Bw',
+  authDomain: 'react-portfolio-dashboar-44560.firebaseapp.com',
+  projectId: 'react-portfolio-dashboar-44560',
+  storageBucket: 'react-portfolio-dashboar-44560.firebasestorage.app',
+  messagingSenderId: '338770020868',
+  appId: '1:338770020868:web:a5918b99a99733158fc351',
+}
 
-// export const signInWithGoogle = () => signInWithPopup(auth, provider)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
